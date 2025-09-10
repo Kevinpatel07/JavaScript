305f7cff5ed028c75a2589aa9b8e92d3359ab100
Kevin Patel <patelkevin782002@gmail.com>
2025-09-10 22:59:05 +0530

    JavaScript

---
+++ Closure/counter.closure.js
@@ -0,0 +1,20 @@
+function createCounter(){
+    var count = 0
+
+    return {
+        increment : function (){
+            count++
+            return count
+        } , 
+        getCount : function (){
+            return count 
+        }
+    }
+}
+
+var counter = createCounter()
+console.log(counter.increment())
+console.log(counter.increment())
+console.log(counter.increment())
+console.log(counter.increment())
+console.log(counter.getCount())
