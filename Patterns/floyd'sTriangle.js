73721de23469b5ecb365ddd0221a2d7f327c5696
Kevin Patel <patelkevin782002@gmail.com>
2025-09-17 14:13:58 +0530

    JavaScript

--- Patterns/floyd'sTriangle.js
+++ Patterns/floyd'sTriangle.js
@@ -1,12 +1,32 @@
+// let n = 4
+// let count  = 0
+
+// for(let i=1; i<=n; i++){
+//     let temp = ""
+//     for(let j=1; j<=i; j++){
+//         count ++
+//         temp += count + " "
+//     }
+//     console.log(temp)
+
+// }
+
+// floyd's triangle pyramid setup
+
 let n = 4
-let count  = 0
+let count = 0
 
 for(let i=1; i<=n; i++){
     let temp = ""
-    for(let j=1; j<=i; j++){
-        count ++
+
+    for(let j=1; j<=n-i; j++){
+        process.stdout.write(" ".toString())
+    }
+
+    for(let k=1; k<=i; k++){
+        count++
         temp += count + " "
     }
-    console.log(temp)
 
+    console.log(temp)
 }
